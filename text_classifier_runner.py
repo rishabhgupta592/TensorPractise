0cdbc51e6f69537dbf3a4f989fc9b7083c971edd
Rishabh.gupta <rishabh.gupta@happiestminds.com>
2017-12-21 21:35:15 +0530

    Using save model to make run time prediction

---
+++ text_classifier_runner.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from sklearn.externals import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+import _pickle as pickle
+
+def load_models(feature):
+    # saver = tf.train.import_meta_graph('./models/text_classifier/model.meta')
+    with tf.Session() as sess:
+        new_saver = tf.train.import_meta_graph('./models/text_classifier/model.ckpt.meta')
+        new_saver.restore(sess, tf.train.latest_checkpoint('./models/text_classifier/'))
+        graph = tf.get_default_graph()
+        # X = tf.placeholder("float", [None, 861], name='X')
+        X = graph.get_tensor_by_name("X:0")
+        prediction = graph.get_tensor_by_name("prediction:0")
+        print(sess.run(prediction, feed_dict={X:feature}))
+if __name__ == "__main__":
+    query = "Work Schedule Discover employees"
+    query = [query]
+    # from sklearn.feature_extraction.text import CountVectorizer
+    # count_vect = joblib.load('feature.pkl')
+    count_vect = CountVectorizer(decode_error="replace", vocabulary=pickle.load(open("feature.pkl", "rb")))
+    X_train_counts = count_vect.fit_transform(query)
+    feature = X_train_counts.toarray()
+    # print(feature)
+    load_models(feature)
